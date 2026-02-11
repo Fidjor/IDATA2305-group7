2cf4d6df1898af91aebd2aca8ff7ef6810c11fb2
Magnus Pedersen Voldsund <magnus.voldsund@outlook.com>
2026-02-11 16:36:40 +0100

    Did some refactoring

--- Calculator/src/main/java/Server/ServerSingleThread.java
+++ Calculator/src/main/java/Server/ServerSingleThread.java
@@ -1,3 +1,5 @@
+package Server;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,7 +7,7 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class ServerSingleThread {
 
   static boolean running = false;
 
