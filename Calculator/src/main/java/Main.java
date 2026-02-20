cb107d847c4bfa5ebffdd9a0c45bb7c3cf008305
Magnus Pedersen Voldsund <magnus.voldsund@outlook.com>
2026-02-20 14:47:15 +0100

    Added average time calculation to main

--- Calculator/src/main/java/Main.java
+++ Calculator/src/main/java/Main.java
@@ -3,23 +3,29 @@ import Client.Client;
 public class Main {
   public static void main(String[] args) throws InterruptedException {
 
-    long startTime = System.nanoTime();
-    Thread[] threads = new Thread[10];
+    int timesToRun = 10;
+    long averageDuration = 0;
 
-    for (int i = 0; i < 10; i++) {
-      threads[i] = new Thread(() -> {
-        Client client = new Client(8080);
-        client.sendMessage(2, 2, '+');
-      });
-      threads[i].start();
+    for (int i = 0; i < timesToRun; i++) {
+      long startTime = System.nanoTime();
+      Thread[] threads = new Thread[10];
+
+      for (int j = 0; j < 10; j++) {
+        threads[j] = new Thread(() -> {
+          Client client = new Client(8080);
+          client.sendMessage(2, 2, '+');
+        });
+        threads[j].start();
+      }
+
+      for (Thread thread : threads) {
+        thread.join();
+      }
+      long duration = (System.nanoTime() - startTime)/1000000;
+      averageDuration += duration;
     }
 
-    for (Thread thread : threads) {
-      thread.join();
-    }
-
-    long duration = (System.nanoTime() - startTime)/1000000;
-    System.out.println(duration + "ms");
+    System.out.println(averageDuration + "ms");
 
   }
 }
